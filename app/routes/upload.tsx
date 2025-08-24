9c7f5db2e84f814f76373544fbb1cd8626d497ec
Pavan47 Kumar <Pavan47.Kumar@ril.com>
2025-08-24 16:43:48 +0530

    Added Upload Section

---
+++ app/routes/upload.tsx
@@ -0,0 +1,104 @@
+import React, { type FormEvent } from "react";
+import Navbar from "~/components/Navbar";
+import { useState } from "react";
+import Fileupload from "~/components/fileupload";
+
+const Upload = () => {
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [statusText, setStatusText] = useState("This is a Status Text");
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleFileSelect = (file: File | null) => {
+    setFile(file);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.currentTarget.closest("form");
+    if (!form) return;
+    const formData = new FormData(form);
+
+    const company_Name = formData.get("company-name") as string;
+    const jobTitle = formData.get("job-title") as string;
+    const jobDescription = formData.get("job-description") as string;
+
+    console.log({
+      company_Name,
+      jobTitle,
+      jobDescription,
+      file,
+    });
+  };
+
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1 className="w-[80%]">Smart feedback for your dream job</h1>
+          {isProcessing ? (
+            <>
+              <h2>{statusText}</h2>
+              <img
+                src="/images/resume-scan.gif"
+                alt="Resume Scan"
+                className="w-full"
+              />
+            </>
+          ) : (
+            <>
+              <h2>Drop Your Resume for an ATS Score and Improvement Tips</h2>
+            </>
+          )}
+
+          {!isProcessing && (
+            <form
+              id="upload-form"
+              onSubmit={handleSubmit}
+              className="flex flex-col gap-4 mt-8"
+            >
+              <div className="form-div">
+                <label htmlFor="company-name">Company Name</label>
+                <input
+                  type="text"
+                  name="company-name"
+                  placeholder="Company Name"
+                  id="company-name"
+                />
+              </div>
+              <div className="form-div">
+                <label htmlFor="job-title">Job Title</label>
+                <input
+                  type="text"
+                  name="job-title"
+                  placeholder="Job Title"
+                  id="job-title"
+                />
+              </div>
+              <div className="form-div">
+                <label htmlFor="job-description">Job Description</label>
+                <textarea
+                  rows={5}
+                  name="job-description"
+                  id="job-description"
+                ></textarea>
+              </div>
+
+              <div className="form-div">
+                <label htmlFor="upload">Upload Resume</label>
+                <Fileupload onFileSelect={handleFileSelect} file={file} />
+              </div>
+
+              <button className="primary-button" type="submit">
+                Analyze Resume
+              </button>
+            </form>
+          )}
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Upload;
